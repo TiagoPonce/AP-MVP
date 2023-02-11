8ea62e08300a363e1e387edb0ecca2025390e11f
Dheniarley Cruz <dheniarley.ds@gmail.com>
2023-02-10 22:20:53 -0300

    finalizando layout tela Dashboard

--- src/components/TableCompany.js
+++ src/components/TableCompany.js
@@ -3,8 +3,8 @@ import PlusSquare from 'public/plus-square.png'
 
 export default function TableCompany() {
     return (
-        <div className="h-[222px] bg-white rounded-lg m-10 flex p-2">
-            <div className="flex w-full h-20 items-center justify-between pl-10 pr-10 border-b-black border-solid border-b-2">
+        <div className="h-[222px] bg-white rounded-lg m-10 flex flex-col gap-8 p-2">
+            <div className="flex w-full items-center justify-between pl-10 pr-10 pb-2 border-b-black border-solid border-b-2">
                 <p>Empresas</p>
                 <button
                 className="w-[214px] h-[44px] border-solid border-2 hover:bg-slate-100 border-slate-600 rounded-full flex items-center justify-center"
@@ -18,6 +18,26 @@ export default function TableCompany() {
                     Adcionar Empresa
                 </button>
             </div>
+            <table className="table-auto mt-4 m-10">
+                <thead className="border-b-2 border-black border-solid mb-4">
+                    <tr>
+                        <th>Empresa</th>
+                        <th>Líderes</th>
+                        <th>Funcionários</th>
+                        <th>Período</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="h-14 hover:bg-[#C1D3ED]/30 cursor-pointer border-solid border-b border-[#9D9A9A]">
+                        <td className="text-center">Info Tech</td>
+                        <td className="text-center">1</td>
+                        <td className="text-center">5</td>
+                        <td className="text-center">02-23 à 03-23</td>
+                        <td className="text-center">Não iniciado</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -10,13 +10,11 @@ export default function Dashboard() {
             <Header />
             <div className="flex h-full w-full">
                 <SideBar />
-                <main className="w-screen h-screen bg-black">
+                <main className="w-screen h-screen bg-[#C1D3ED]/30">
                     <div className="w-full h-14 bg-[#A4BBDD] mt-20 flex items-center">
                         <p className="text-base ml-20">Dashboard</p>
                     </div>
-                  
                     <TableCompany/>
-                    
                 </main>
             </div>
             <Footer />
